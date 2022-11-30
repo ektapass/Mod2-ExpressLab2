929d63eb6a809890c9c702988c6851c05ac34990
Ekta Bharti <ektabharti019@gmail.com>
2022-11-30 14:29:48 -0500

    third commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/greeting/:name',function(req,res)  {
   });
 
     
-  app.listen(3000, function() {
+  app.listen(3000, function() { 
     console.log('Listening on port 3000');
    });
 
@@ -27,3 +27,20 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 	const tip = (req.params.total*req.params.tipPercentage)/100
 	res.send(' your tip will be:'+tip)
 });
+
+//magic 8 ball
+app.get('/magic/:questionasked',(req,res)=>{
+let magic8Ball=["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+"You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes",
+ "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now",
+  "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no",
+  "Outlook not so good", "Very doubtful"]
+  const questionAsked= req.params.questionasked
+  let randomAnswer = magic8Ball[Math.floor(Math.random()*magic8Ball.length)]
+  res.send(questionAsked+ "?"+ `<h1> ${randomAnswer}</h1>`)
+
+
+}
+)
+
+
